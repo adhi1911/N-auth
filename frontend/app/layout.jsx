ad247fe32ad580ed3102fa5d27266f3a5f086e9c
adhi1911 <amk2754@gmail.com>
2025-09-30 11:46:12 +0530

    fix: Hydration error

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,18 +1,21 @@
 import Navbar from './components/Navbar'
+import { Inter } from 'next/font/google'
 import { AuthProvider } from '@/context/AuthContext'
 import './globals.css'
 
 
 
+const inter = Inter({ subsets: ['latin'] })
+
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>
+    <html lang="en">
+      <body className={inter.className} suppressHydrationWarning={true}>
         <AuthProvider>
           <Navbar />
           {children}
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
